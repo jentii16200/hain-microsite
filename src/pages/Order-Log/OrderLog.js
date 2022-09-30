c06842d7fc2d71e4d48da7b1676fa975bcbe40df
SAGS0000 <113842812+SAGS0000@users.noreply.github.com>
2022-09-30 22:33:25 +0800

    feat: orderLog

--- src/pages/Order-Log/OrderLog.js
+++ src/pages/Order-Log/OrderLog.js
@@ -1,9 +1,40 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Heading } from '@chakra-ui/react';
+import axios from 'axios';
+import { Table, Thead, Tbody, Tfoot, Tr, Th, Td, TableCaption,
+    TableContainer } from '@chakra-ui/react';
 export const OrderLog = () => {
+    const [posts, setPosts] = useState([]);
+    const apiEndPoint = '';
+    useEffect(() => {
+        axios.get(apiEndPoint).then(res => {
+            console.log(res);
+            setPosts(res.data);
+        }).catch(err =>{
+            console.log(err);
+        });
+    }, []);
     return (
         <div>
             <Heading className='title'>ORDER LOG</Heading>
+            <TableContainer className='table'>
+                <Table size='sm'>
+                    <Thead>
+                        <Tr>
+                            <Th>ORDERLOG #</Th>
+                            <Th>REMARKS</Th>
+                            <Th isNumeric>DELETE</Th>
+                        </Tr>
+                    </Thead>
+                    <Tbody>
+                        {posts.map(post =>
+                            <Tr key={post.inquiryNumber}>
+                                <Td>{post.inquiryNumber}</Td>
+                                <Td>{post.remarks}</Td>
+                            </Tr>)}
+                    </Tbody>
+                </Table>
+            </TableContainer>
         </div>
     );
 };
