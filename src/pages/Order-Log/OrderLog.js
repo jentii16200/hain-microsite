685aae48ab85760d25c2b1a04ddc368c896b048f
SAGS0000 <113842812+SAGS0000@users.noreply.github.com>
2022-10-02 13:25:04 +0800

    feat: temp OrderLog

--- src/pages/Order-Log/OrderLog.js
+++ src/pages/Order-Log/OrderLog.js
@@ -3,17 +3,55 @@ import { Heading } from '@chakra-ui/react';
 import axios from 'axios';
 import { Table, Thead, Tbody, Tfoot, Tr, Th, Td, TableCaption,
     TableContainer } from '@chakra-ui/react';
+
 export const OrderLog = () => {
-    const [posts, setPosts] = useState([]);
-    const apiEndPoint = '';
-    useEffect(() => {
-        axios.get(apiEndPoint).then(res => {
-            console.log(res);
-            setPosts(res.data);
-        }).catch(err =>{
-            console.log(err);
-        });
-    }, []);
+    // const [posts, setPosts] = useState([]);
+    // const apiEndPoint = '';
+    // useEffect(() => {
+    //     axios.get(apiEndPoint).then(res => {
+    //         console.log(res);
+    //         setPosts(res.data);
+    //     }).catch(err =>{
+    //         console.log(err);
+    //     });
+    // }, []);
+    const orderLogData = [
+        {
+            id: '1',
+            name: 'SAGS',
+            total: '500',
+            date: '12.2.2022',
+
+        },
+        {
+            id: '2',
+            name: 'PASCY',
+            total: '150',
+            date: '12.2.2022',
+
+        },
+        {
+            id: '3',
+            name: 'JORGE',
+            total: '503',
+            date: '12.1.2022',
+
+        },
+        {
+            id: '4',
+            name: 'SINDINGAN',
+            total: '800',
+            date: '12.1.2022',
+
+        },
+        {
+            id: '5',
+            name: 'JM',
+            total: '10',
+            date: '12.1.2022',
+
+        },
+    ];
     return (
         <div>
             <Heading className='title'>ORDER LOG</Heading>
@@ -22,15 +60,18 @@ export const OrderLog = () => {
                     <Thead>
                         <Tr>
                             <Th>ORDERLOG #</Th>
-                            <Th>REMARKS</Th>
-                            <Th isNumeric>DELETE</Th>
+                            <Th>NAME</Th>
+                            <Th>TOTAL</Th>
+                            <Th>DATE</Th>
                         </Tr>
                     </Thead>
                     <Tbody>
-                        {posts.map(post =>
-                            <Tr key={post.inquiryNumber}>
-                                <Td>{post.inquiryNumber}</Td>
-                                <Td>{post.remarks}</Td>
+                        {orderLogData.map(post =>
+                            <Tr key={post.id}>
+                                <Td>{post.id}</Td>
+                                <Td>{post.name}</Td>
+                                <Td>{post.total}</Td>
+                                <Td>{post.date}</Td>
                             </Tr>)}
                     </Tbody>
                 </Table>
