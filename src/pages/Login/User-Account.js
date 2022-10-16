fc46f11a7695b1bbb78f3aa56f9b5b0c598bb511
SAGS0000 <113842812+SAGS0000@users.noreply.github.com>
2022-10-16 11:50:18 +0800

    loading

--- src/pages/Login/User-Account.js
+++ src/pages/Login/User-Account.js
@@ -1,11 +1,11 @@
 import React, { useEffect, useState } from 'react';
-import { Center, Wrap, WrapItem, Box, Container, FormControl, FormLabel, Button, Input, useToast, } from '@chakra-ui/react';
+import { Center, Wrap, WrapItem, Box, Container, FormControl, FormLabel, Button, Input, useToast, Spinner } from '@chakra-ui/react';
 import '../../assets/index.css';
 import { Navigate } from 'react-router-dom';
 import BackGround from '../../assets/bg.png';
 
 export const UserAccount = (props) => {
-
+    const [isLoading, setIsLoading] = useState(true);
     const toast = useToast();
     const [goToContact, setGoToContact] = useState(false);
     const [email, setEmail] = useState('');
@@ -14,25 +14,38 @@ export const UserAccount = (props) => {
     const handlePassword = (e) => setPassword(e.target.value);
     const isErrorEmail = email === '';
     const isErrorPass = password === '';
+
     function isAccountExist(){
         if(email != '' && password != ''){
-            setGoToContact(true);
-            toast({
-                title: 'Successfully login',
-                description: 'Welcome user!',
-                status: 'success',
-                duration: 1000,
-                isClosable: false,
-                position: 'top-right'
+            new Promise(resolve => {
+                setIsLoading(false);
+                setTimeout(resolve, 1000);
+            }).then(() => {
+                setGoToContact(true);
+                toast({
+                    title: 'Successfully login',
+                    description: 'Welcome user!',
+                    status: 'success',
+                    duration: 1000,
+                    isClosable: false,
+                    position: 'top-right'
+                });
             });
+
         }else{
-            toast({
-                title: 'Incorrect username/password',
-                description: 'Please put the right information',
-                status: 'error',
-                duration: 1000,
-                isClosable: false,
-                position: 'top-right'
+            new Promise(resolve => {
+                setIsLoading(false);
+                setTimeout(resolve, 1000);
+            }).then(() => {
+                setIsLoading(true);
+                toast({
+                    title: 'Incorrect username/password',
+                    description: 'Please put the right information',
+                    status: 'error',
+                    duration: 1000,
+                    isClosable: false,
+                    position: 'top-right'
+                });
             });
         }
     }
@@ -42,41 +55,54 @@ export const UserAccount = (props) => {
 
     }
     return(
-        <div className='bg'>
-            <Box h='calc(25vh)' />
-            <Wrap justify='center'>
-                <Wrap Item>
-                    <Box w='calc(50vw)' pl = "10" pr = "10">
-                        <Center bg="rgba(0,0,0,0.1)" w='300' h='300' color='white' >
-                            <Container color='white'>
-                                <FormControl isInvalid={isErrorEmail}>
-                                    <FormLabel>Email</FormLabel>
-                                    <Input
-                                        type='email'
-                                        value={email}
-                                        onChange={handleEmail}
-                                    />
-                                </FormControl>
-                                <FormControl isInvalid={isErrorPass}>
-                                    <FormLabel>Password</FormLabel>
-                                    <Input
-                                        type='password'
-                                        value={password}
-                                        onChange={handlePassword}
-                                    />
-                                </FormControl>
-                                <Center>
-                                    <Button colorScheme='teal' variant='solid' size='lg' mt='4' pl="20" pr="20" onClick={isAccountExist}>
-                                        Login
-                                    </Button>
-                                </Center>
-                            </Container>
-                        </Center>
-                    </Box>
+        isLoading ?
+            <div className='bg'>
+                <Box h='calc(25vh)' />
+                <Wrap justify='center'>
+                    <Wrap Item>
+                        <Box w='calc(50vw)' pl = "10" pr = "10">
+                            <Center bg="rgba(0,0,0,0.1)" w='300' h='300' color='white' >
+                                <Container color='white'>
+                                    <FormControl isInvalid={isErrorEmail}>
+                                        <FormLabel>Email</FormLabel>
+                                        <Input
+                                            type='email'
+                                            value={email}
+                                            onChange={handleEmail}
+                                        />
+                                    </FormControl>
+                                    <FormControl isInvalid={isErrorPass}>
+                                        <FormLabel>Password</FormLabel>
+                                        <Input
+                                            type='password'
+                                            value={password}
+                                            onChange={handlePassword}
+                                        />
+                                    </FormControl>
+                                    <Center>
+                                        <Button colorScheme='teal' variant='solid' size='lg' mt='4' pl="20" pr="20" onClick={isAccountExist}>
+                                            Login
+                                        </Button>
+                                    </Center>
+                                </Container>
+                            </Center>
+                        </Box>
+                    </Wrap>
                 </Wrap>
+                <Box h='calc(33vh)'/>
+            </div>
+            :
+            <Wrap justify='Center'>
+                <Box pt='20%'>
+                    <Spinner
+                        thickness='4px'
+                        speed='0.65s'
+                        emptyColor='gray.200'
+                        color='blue.500'
+                        size='xl'
+                    />
+                </Box>
             </Wrap>
-            <Box h='calc(33vh)'/>
-        </div>
 
     );
 };
