6d92fbb1c47aa401650e255588b7656776b61086
SAGS0000 <113842812+SAGS0000@users.noreply.github.com>
2022-10-17 13:40:51 +0800

    update: modal (MENU)

--- src/pages/Menu-Management/Pulutan.js
+++ src/pages/Menu-Management/Pulutan.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Box, Image, Badge, Tooltip, useDisclosure, Button } from '@chakra-ui/react';
+import { Box, Image, Badge, Tooltip, useDisclosure, Button, Grid, GridItem } from '@chakra-ui/react';
 import axios from 'axios';
 import { MenuLabel } from './testing/ToolTip';
 import { HoverModal } from './testing/HoverModal';
@@ -52,16 +52,37 @@ export const Pulutan = () => {
                             }}>
                             { modalIsOpen &&
                                 <Modal
+                                    size='3xl'
                                     isOpen={modalIsOpen}
                                     onClose={() => setModalIsOpen(false)}>
                                     <ModalOverlay />
                                     <ModalContent>
-                                        <ModalHeader>
-                                            {modalData.name}
-                                        </ModalHeader>
-                                        <ModalCloseButton />
                                         <ModalBody>
-                                            {modalData.description}
+                                            <Grid
+                                                templateRows={'repeat(5, 1fr)'}
+                                                templateColumns={'repeat(5, 1fr)'}
+                                                gap={'1'}
+                                                h='500px'>
+                                                <GridItem rowSpan={2} colSpan={3}
+                                                    bg='gray'>
+                                                    {modalData.name}
+                                                </GridItem>
+                                                <GridItem rowSpan={2} colSpan={2}
+                                                    bg='gray'>
+                                                    <Image src={modalData.imageUrl}
+                                                        width='100%'
+                                                        height='100%'
+                                                        objectFit='cover' />
+                                                </GridItem>
+                                                <GridItem rowSpan={3} colSpan={1}
+                                                    bg='gray'>
+                                                    TESTING
+                                                </GridItem>
+                                                <GridItem rowSpan={3} colSpan={4}
+                                                    bg='gray'>
+                                                    {modalData.description}
+                                                </GridItem>
+                                            </Grid>
                                         </ModalBody>
                                         <ModalFooter>
                                             <Button
