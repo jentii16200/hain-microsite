e9d4be16986f972ade09e5e0cc11ca7310c4bf46
SAGS0000 <113842812+SAGS0000@users.noreply.github.com>
2023-03-15 12:41:35 +0800

    Update: getting date and time

--- src/pages/Register-Employee/RegisterEmployee.js
+++ src/pages/Register-Employee/RegisterEmployee.js
@@ -1,11 +1,14 @@
-import React from 'react';
-import { Heading } from '@chakra-ui/react';
+import React, { useState } from 'react';
+import { Button, Heading } from '@chakra-ui/react';
 import RegisterEmployeeForms from './RegisterEmployeeForms';
+import CurrentDateTime from '../../components/CurrentDateTime';
 const RegisterEmployee = () => {
+    // const [dateTime, setDateTime] = useState("");
     return (
         <React.Fragment>
             <Heading className='title'>REGISTER EMPLOYEE</Heading>
             <RegisterEmployeeForms />
+            {/* <Button onClick={() => setDateTime(CurrentDateTime())}> {dateTime}</Button> */}
         </React.Fragment>
     );
 };
