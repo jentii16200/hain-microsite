73f713f11f31e5b807e37830971d597cabd93d7e
SAGS0000 <113842812+SAGS0000@users.noreply.github.com>
2023-03-23 11:28:44 +0800

    My Account Updates

---
+++ src/pages/My-Account/components/UpdateAlertDialog.js
@@ -0,0 +1,67 @@
+import React from 'react';
+import {
+    AlertDialog,
+    AlertDialogBody,
+    AlertDialogFooter,
+    AlertDialogHeader,
+    AlertDialogContent,
+    AlertDialogOverlay,
+    useDisclosure,
+    Button,
+    useToast,
+} from '@chakra-ui/react';
+
+const UpdateAlertDialog = ({ userInfo }) => {
+    const { isOpen, onOpen, onClose } = useDisclosure();
+    const cancelRef = React.useRef();
+    const toast = useToast();
+
+    const HandleAccept = () => {
+        console.log(userInfo);
+    };
+    return (
+        <>
+            <Button colorScheme='green' onClick={onOpen}>
+                Update
+            </Button>
+            <AlertDialog
+                isOpen={isOpen}
+                leastDestructiveRef={cancelRef}
+                onClose={onClose}
+            >
+                <AlertDialogOverlay>
+                    <AlertDialogContent>
+                        <AlertDialogHeader fontSize='lg' fontWeight='bold'>
+                            Update Information
+                        </AlertDialogHeader>
+                        <AlertDialogBody>
+                            Are you sure you will update your information?
+                        </AlertDialogBody>
+                        <AlertDialogFooter>
+                            <Button ref={cancelRef} onClick={onClose}>
+                                Cancel
+                            </Button>
+                            <Button colorScheme='green'
+                                onClick={() => {
+                                    toast({
+                                        title: 'Account Updated.',
+                                        description: "We've successfully updated your account.",
+                                        position: 'top-right',
+                                        status: 'success',
+                                        duration: 2000,
+                                        isClosable: true,
+                                    });
+                                    onClose();
+                                    HandleAccept();
+                                }} ml={3}>
+                                Update
+                            </Button>
+                        </AlertDialogFooter>
+                    </AlertDialogContent>
+                </AlertDialogOverlay>
+            </AlertDialog>
+        </>
+    );
+};
+
+export default UpdateAlertDialog;
