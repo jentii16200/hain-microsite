be5b67afa81171218ad6b4d6285bb5485f1b9975
SAGS0000 <113842812+SAGS0000@users.noreply.github.com>
2023-03-23 11:29:47 +0800

    Revert "My Account Updates"
    This reverts commit 73f713f11f31e5b807e37830971d597cabd93d7e.

--- src/pages/My-Account/MyAccount.js
+++ src/pages/My-Account/MyAccount.js
@@ -1,135 +1,13 @@
-import {
-    Card, Text,
-    Heading,
-    Flex,
-    FormControl,
-    FormLabel,
-    Input,
-    Button,
-    Divider
-} from '@chakra-ui/react';
-import React, { useState } from 'react';
-import UpdateAlertDialog from './components/UpdateAlertDialog';
+import { Heading } from '@chakra-ui/react';
+import React from 'react';
 
 const MyAccount = () => {
     const storedUser = JSON.parse(localStorage.getItem('currentUser'));
     console.log(storedUser);
-
-    const [user, setUser] = useState(storedUser);
-
-    const HandleUpdate = (e) => {
-        setUser((currentValue) => ({ ...currentValue, [e.target.name]: e.target.value }));
-    };
-    console.log(user);
     return (
-        <>
+        <div>
             <Heading className='title'>MY ACCOUNT</Heading>
-            <Flex
-                backgroundColor='gray.50'
-                flexDirection='column'
-                padding='10'>
-                <Flex justifyContent='space-between'>
-                    <Flex flexDirection='column'>
-                        <Text fontSize='20px' as='b'>Account Settings</Text>
-                        <Text textColor='gray'>Change your profile and account settings</Text>
-                    </Flex>
-                    <UpdateAlertDialog userInfo={user} />
-                </Flex>
-                <Card
-                    variant='elevated'
-                    backgroundColor='white'
-                    borderRadius='xl'
-                    marginTop='5'
-                    marginInline='10'
-                    padding='5'>
-                    <Flex justifyContent='space-between'>
-                        <Text>
-                            Account ID
-                        </Text>
-                        <Text fontSize='20px'>
-                            {user.accountID}
-                        </Text>
-                    </Flex>
-                    <Divider colorScheme='teal' />
-                    <Text fontSize='15px'
-                        as='b'
-                        marginBlock='2.5'>General Information
-                    </Text>
-                    <Flex gap='5'>
-                        <FormControl mb={4}>
-                            <FormLabel
-                                fontSize='15'>
-                                First Name
-                            </FormLabel>
-                            <Input
-                                name='employeeFirstName'
-                                value={user.employeeFirstName}
-                                placeholder="Enter your first name"
-                                onChange={HandleUpdate} />
-                        </FormControl>
-                        <FormControl mb={4}>
-                            <FormLabel
-                                fontSize='15'>
-                                Last Name
-                            </FormLabel>
-                            <Input
-                                name='employeeLastName'
-                                value={user.employeeLastName}
-                                placeholder="Enter your last name"
-                                onChange={HandleUpdate} />
-                        </FormControl>
-                    </Flex>
-                    <FormControl mb={4}>
-                        <FormLabel
-                            fontSize='15'>
-                            Address
-                        </FormLabel>
-                        <Input
-                            name='address'
-                            value={user.address}
-                            placeholder="Enter your address"
-                            onChange={HandleUpdate} />
-                    </FormControl>
-                    <Flex gap='5'>
-                        <FormControl mb={4}>
-                            <FormLabel
-                                fontSize='15'>
-                                E-Mail
-                            </FormLabel>
-                            <Input
-                                name='email'
-                                value={user.email}
-                                placeholder="Enter your email"
-                                onChange={HandleUpdate} />
-                        </FormControl>
-                        <FormControl mb={4}>
-                            <FormLabel
-                                fontSize='15'>
-                                Contact #
-                            </FormLabel>
-                            <Input
-                                name='contactNum'
-                                value={user.contactNum}
-                                placeholder="Enter your number"
-                                onChange={HandleUpdate} />
-                        </FormControl>
-                    </Flex>
-                    <FormControl
-                        mb={4}>
-                        <FormLabel
-                            fontSize='15'>
-                            Password
-                        </FormLabel>
-                        <Input
-                            isRequired
-                            name='password'
-                            value={user.password}
-                            placeholder="Enter your password"
-                            onChange={HandleUpdate} />
-                    </FormControl>
-                </Card>
-            </Flex>
-        </>
+        </div>
     );
 };
 export default MyAccount;
