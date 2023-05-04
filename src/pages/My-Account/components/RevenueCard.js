b675c7f71b48077719b7cfdb3914b698ff2c7e7a
SAGS0000 <113842812+SAGS0000@users.noreply.github.com>
2023-05-04 21:20:22 +0800

    up rev

--- src/pages/My-Account/components/RevenueCard.js
+++ src/pages/My-Account/components/RevenueCard.js
@@ -1,6 +1,7 @@
 import { Box, Card, CardBody, Flex, Text } from '@chakra-ui/react';
 import React from 'react';
 import { VscGraphLine } from 'react-icons/vsc';
+import { Area, AreaChart, Tooltip, XAxis, YAxis, CartesianGrid, ResponsiveContainer } from 'recharts';
 
 export const RevenueCard = ({ posts }) => {
     let totalRevenue = 0;
@@ -9,13 +10,65 @@ export const RevenueCard = ({ posts }) => {
             totalRevenue += parseInt(post.totalPrice);
         }
     });
+
+    const data = [
+        {
+            name: "Page A",
+            uv: 4000,
+            pv: 2400,
+            amt: 2400
+        },
+        {
+            name: "Page B",
+            uv: 3000,
+            pv: 1398,
+            amt: 2210
+        },
+        {
+            name: "Page C",
+            uv: 2000,
+            pv: 9800,
+            amt: 2290
+        },
+        {
+            name: "Page D",
+            uv: 2780,
+            pv: 3908,
+            amt: 2000
+        },
+        {
+            name: "Page E",
+            uv: 1890,
+            pv: 4800,
+            amt: 2181
+        },
+        {
+            name: "Page F",
+            uv: 2390,
+            pv: 3800,
+            amt: 2500
+        },
+        {
+            name: "Page G",
+            uv: 3490,
+            pv: 4300,
+            amt: 2100
+        },
+        {
+            name: "Page H",
+            uv: 3490,
+            pv: 4300,
+            amt: 2100
+        }
+    ];
+
     return (
         <Card minHeight="100%" minWidth="50px">
-            <CardBody>
+            <CardBody >
                 <Text fontSize='xl' fontFamily='monospace' fontWeight='bold' color='black'>
                     Revenue
                 </Text>
-                <Flex >
+                <Flex>
                     <Flex flexDirection='column'
                         marginTop='10'
                         marginLeft='5'>
@@ -26,6 +79,25 @@ export const RevenueCard = ({ posts }) => {
                             Total Revenue
                         </Text>
                     </Flex>
+                    <Box width='100%' minHeight='100%'>
+                        <ResponsiveContainer width="100%" minHeight="200px" >
+                            <AreaChart data={data}
+                                // width={630} height={250}
+                                margin={{ top: 10, right: 30, left: 0, bottom: 0 }}>
+                                <defs>
+                                    <linearGradient id="colorPv" x1="0" y1="0" x2="0" y2="1">
+                                        <stop offset="5%" stopColor="#82ca9d" stopOpacity={0.8} />
+                                        <stop offset="95%" stopColor="#82ca9d" stopOpacity={0} />
+                                    </linearGradient>
+                                </defs>
+                                <XAxis dataKey="name" />
+                                <YAxis />
+                                <CartesianGrid strokeDasharray="3 3" />
+                                <Tooltip />
+                                <Area type="monotone" dataKey="pv" stroke="#82ca9d" fillOpacity={1} fill="url(#colorPv)" />
+                            </AreaChart>
+                        </ResponsiveContainer>
+                    </Box>
                 </Flex>
             </CardBody>
         </Card>
