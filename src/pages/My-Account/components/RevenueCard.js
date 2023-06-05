095c00e163ef13fb4b02eedd564facafde411f02
pascccy <kyle.1pascual@gmail.com>
2023-06-05 18:34:56 +0800

    Update RevenueCard.js

--- src/pages/My-Account/components/RevenueCard.js
+++ src/pages/My-Account/components/RevenueCard.js
@@ -4,64 +4,82 @@ import { VscGraphLine } from 'react-icons/vsc';
 import { Area, AreaChart, Tooltip, XAxis, YAxis, CartesianGrid, ResponsiveContainer } from 'recharts';
 
 export const RevenueCard = ({ posts }) => {
+
     let totalRevenue = 0;
     posts?.map(post => {
         if (post.status == 'completed') {
             totalRevenue += parseInt(post.totalPrice);
         }
     });
+    // const data = [
+    //     {
+    //         name: "Page A",
+    //         pv: 2400,
+    //         amt: 2400
+    //     },
+    //     {
+    //         name: "Page B",
+    //         pv: 1398,
+    //         amt: 2210
+    //     },
+    //     {
+    //         name: "Page C",
+    //         pv: 9800,
+    //         amt: 2290
+    //     },
+    //     {
+    //         name: "Page D",
+    //         pv: 3908,
+    //         amt: 2000
+    //     },
+    //     {
+    //         name: "Page E",
+    //         pv: 4800,
+    //         amt: 2181
+    //     },
+    //     {
+    //         name: "Page F",
+    //         pv: 3800,
+    //         amt: 2500
+    //     },
+    //     {
+    //         name: "Page G",
+    //         pv: 4300,
+    //         amt: 2100
+    //     },
+    //     {
+    //         name: "Page H",
+    //         pv: 4300,
+    //         amt: 2100
+    //     }
+    // ];
 
-    const data = [
-        {
-            name: "Page A",
-            uv: 4000,
-            pv: 2400,
-            amt: 2400
-        },
-        {
-            name: "Page B",
-            uv: 3000,
-            pv: 1398,
-            amt: 2210
-        },
-        {
-            name: "Page C",
-            uv: 2000,
-            pv: 9800,
-            amt: 2290
-        },
-        {
-            name: "Page D",
-            uv: 2780,
-            pv: 3908,
-            amt: 2000
-        },
-        {
-            name: "Page E",
-            uv: 1890,
-            pv: 4800,
-            amt: 2181
-        },
-        {
-            name: "Page F",
-            uv: 2390,
-            pv: 3800,
-            amt: 2500
-        },
-        {
-            name: "Page G",
-            uv: 3490,
-            pv: 4300,
-            amt: 2100
-        },
-        {
-            name: "Page H",
-            uv: 3490,
-            pv: 4300,
-            amt: 2100
-        }
-    ];
+    // const chartData = [];
+    // posts?.forEach(item => {
+    //   const date = item.currentDate?.split('/')[1];
+    //   const amount = parseInt(item.totalPrice);
+  
+    //   const existingData = chartData.find(d => d.date === date);
+    //   if (existingData) {
+    //     existingData.revenue += amount;
+    //   } else {
+    //     chartData.push({ date, revenue: amount });
+    //   }
+    // });
 
+    const chartData = [];
+  const currentDate = new Date();
+  const currentMonth = currentDate.getMonth();
+  const currentYear = currentDate.getFullYear();
+  const daysInMonth = new Date(currentYear, currentMonth + 1, 0).getDate();
+
+  for (let i = 1; i <= daysInMonth; i++) {
+    const day=i;
+    const date = `${i}/${currentMonth + 1}/${currentYear}`;
+    const revenue = posts?.filter(item => item.currentDate === date && item.status === 'completed')
+      .reduce((sum, item) => sum + parseInt(item.totalPrice), 0);
+    chartData.push({ day,date, revenue });
+  }
     return (
         <Card minHeight="100%" minWidth="50px">
             <CardBody >
@@ -81,7 +99,7 @@ export const RevenueCard = ({ posts }) => {
                     </Flex>
                     <Box width='100%' minHeight='100%'>
                         <ResponsiveContainer width="100%" minHeight="200px" >
-                            <AreaChart data={data}
+                            <AreaChart data={chartData}
                                 // width={630} height={250}
                                 margin={{ top: 10, right: 30, left: 0, bottom: 0 }}>
                                 <defs>
@@ -90,11 +108,11 @@ export const RevenueCard = ({ posts }) => {
                                         <stop offset="95%" stopColor="#82ca9d" stopOpacity={0} />
                                     </linearGradient>
                                 </defs>
-                                <XAxis dataKey="name" />
+                                <XAxis dataKey='day' />
                                 <YAxis />
                                 <CartesianGrid strokeDasharray="3 3" />
                                 <Tooltip />
-                                <Area type="monotone" dataKey="pv" stroke="#82ca9d" fillOpacity={1} fill="url(#colorPv)" />
+                                <Area type="monotone" dataKey="revenue" stroke="#82ca9d" fillOpacity={1} fill="url(#colorPv)" />
                             </AreaChart>
                         </ResponsiveContainer>
                     </Box>
